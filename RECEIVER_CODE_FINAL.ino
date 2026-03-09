8914ebc3ec70fae3a01199b0b06a8a19c0db6318
Jason Do <153405342+Dalsokajason@users.noreply.github.com>
2026-03-08 23:05:40 -0400

    Refactor IR sender and color code management
    Refactor IR transmission code and improve color handling.

--- RECEIVER_CODE_FINAL.ino
+++ RECEIVER_CODE_FINAL.ino
@@ -1,43 +1,26 @@
+#include <IRremote.hpp>
 #include <esp_now.h>
 #include <WiFi.h>
-#include <IRremote.hpp>
 
-//this code will send the current values of the
-constexpr uint8_t IR_LED_PIN = 2;   // change if needed
-const uint16_t address = 0x00BB; //this is the address of Earth
-const uint8_t AntennaCodes[4] = {0x00, 0x30, 0x50, 0x60};//this info can be found the guidelines
-const char* AntennaNum[4] = {"ant1", "ant2", "ant3", "ant4"};
+constexpr uint8_t IR_LED_PIN = 2;
+uint8_t colorCodes[4] = {0x09, 0x0A, 0x0C, 0x0F}; //Red, Green, Blue, Purple
+const char* colorNames[] = {"RED", "GREEN", "BLUE", "PURPLE"};
+uint8_t antennaCodes[4] = {0x00, 0x30, 0x50, 0x60}; //1, 2, 3, 4
+int antennaNames[] = {1, 2, 3, 4};
 
-const uint8_t ColorCodes[4] = {0x09, 0x0A, 0x0C, 0x0F};
-const char* ColorName[4] = {"red", "green", "blue", "purple"};
-
-const int numofsends = 3;
-const int gapsofcombos = 800;
-
-uint8_t makeCommand(wint_t antenna, uint8_t color){
-  return(antenna|color);
-}
-void sendCombo(uint8_t antennaIdx, uint8_t colorIdx) {
-  uint8_t ant = AntennaCodes[antennaIdx];
-  uint8_t col = ColorCodes[colorIdx];
-  uint8_t cmd = makeCommand(ant, col);
- 
-  Serial.print("Sending NEC -> ");
-  Serial.print(AntennaCodes[antennaIdx]);
-  Serial.print("+");
-  Serial.print(ColorName[antennaIdx]);
-  Serial.print(" | address 0x");
-  Serial.print(address, HEX);
-  Serial.print(" command 0x");
-  Serial.println(cmd, HEX);
-
-
-  for (int r = 0; r < numofsends; r++){
-    IrSender.sendNEC(address,cmd, 0);
-    delay(200);//gap between the repeats
+void sendIRData(uint8_t address, uint8_t IRdata) {
+  for (int i = 0; i <= 9; i++) {
+    IrSender.sendNEC(address, IRdata, 0);
+    delay(2000);
   }
+  Serial.println("RAHHH!!! Sent!");
 }
+  //uint16_t address = 0xBB; // NEC 16-bit address
 
+  //Serial.print("Sending NEC -> address 0x");
+  //Serial.print(address, HEX);
+  //Serial.print(" command 0x");
+  //Serial.println(antnum3, HEX);
 
 // Structure example to receive data
 // Must match the sender structure
@@ -46,10 +29,8 @@ typedef struct struct_message {
     int antenna;
 } struct_message;
 
-
 // Create a struct_message called transmission
 struct_message transmission;
-
 
 // callback function that will be executed when data is received
 void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
@@ -60,16 +41,30 @@ void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, in
   Serial.println(transmission.color);
   Serial.print("Antenna: ");
   Serial.println(transmission.antenna);
+  uint8_t IRdata;
+  uint8_t antennaData;
+  uint8_t colorData;
   for(int i = 0; i < 4; i++){
-    if(strcasecmp(ColorName[i], transmission.color) == 0){
-        sendCombo(transmission.antenna, i);
+    if(strcmp(transmission.color, colorNames[i]) == 0){
+      colorData = colorCodes[i];
+      for(int j = 0; j < 4; j++){
+        if(transmission.antenna == antennaNames[j])
+        antennaData = antennaCodes[j];
+      }
     }
   }
+
+  IRdata = antennaData + colorData;
+  sendIRData(0xBB, IRdata);
 }
- 
+
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
+  delay(500);
+
+  esp_now_register_recv_cb(OnDataRecv);
+  // Start IR transmitter on selected pin
+  IrSender.begin(IR_LED_PIN, ENABLE_LED_FEEDBACK);
  
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -83,17 +78,9 @@ void setup() {
  
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv);
-
-  delay(100);
-
-
-  // Start IR transmitter on selected pin
-  IrSender.begin(IR_LED_PIN, ENABLE_LED_FEEDBACK);
   
-  
-
 }
+void loop() {
 
-void loop() {//if we want it to loop forever move the for loops in here
+
 }
