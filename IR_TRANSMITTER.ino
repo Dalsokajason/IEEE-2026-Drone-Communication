8c8fd3b286426139363df8c933c8f4e89ce85cef
Jason Do <153405342+Dalsokajason@users.noreply.github.com>
2026-03-08 21:42:05 -0400

    Refactor IR transmitter code and update parameters
    Refactor IR transmitter code to simplify antenna and color management. Update address and remove unused variables and functions.

--- IR_TRANSMITTER.ino
+++ IR_TRANSMITTER.ino
@@ -1,60 +1,35 @@
 #include <IRremote.hpp>
-//this code will send the current values of the
+
 constexpr uint8_t IR_LED_PIN = 2;   // change if needed
-const uint16_t address = 0x00BB; //this is the address of Earth
-const unit8_t AntennaCodes[4] = {0x00, 0x30, 0x50, 0x60};//this info can be found the guidelines
-const char* AntennaNum[4] = {"ant1", "ant2", "ant3", "ant4"};
-
-
-const unit8_t ColorCodes[4] = {0x09, 0x0A, 0x0C, 0x0F};
-const char* ColorName[4] = {"red", "green", "blue", "purple"};
-
-
-const int numofsends = 3;
-const int gapsofcombos = 800;
-
-
-unit8_t makeCommand(unit_t antenna, unit8_t color){
-  return(antenna|color);
-}
-void sendCombo(uint8_t antennaIdx, uint8_t colorIdx) {
-  uint8_t ant = antennaCodes[antennaIdx];
-  uint8_t col = colorCodes[colorIdx];
-  uint8_t cmd = makeCommand(ant, col);
- 
-  Serial.print("Sending NEC -> ");
-  Serial.print(AntennaCodes, [antennaIdx]);
-  Serial.print("+");
-  Serial.print(ColorName, [antennaIdx]);
-  Serial.print(" | address 0x");
-  Serial.print(address, HEX);
-  Serial.print(" command 0x");
-  Serial.println(cmd, HEX);
-
-
-  for (int r = 0; r < numofsends; r++){
-    IrSender.sendNEC(address,cmd, 0);
-    delay(200);//gap between the repeats
-  }
-}
-
+uint16_t address = 0xBB;
+uint8_t antnum1 = 0x09;
+uint8_t antnum2 = 0x3A;
+uint8_t antnum3 = 0x5C;
+uint8_t antnum4 = 0x6F;
 
 void setup() {
-  Serial.begin(115200);//sets the bit rate
-  delay(100);
-
+  Serial.begin(115200);
+  delay(500);
 
   // Start IR transmitter on selected pin
   IrSender.begin(IR_LED_PIN, ENABLE_LED_FEEDBACK);
- 
-  for (int i = 0; i <= 4; i++)
+
+  //uint16_t address = 0xBB; // NEC 16-bit address
+  //uint8_t command  = 0x5C;   // 0x50 + 0x0C = 0x5C
+
+  Serial.print("Sending NEC -> address 0x");
+  Serial.print(address, HEX);
+  Serial.print(" command 0x");
+  Serial.println(antnum3, HEX);
+
+  // 3rd param = repeats (0 = send once)
+  for (int i = 0; i <= 9; i++)
   {
-    for int j = 0; c < 4; c++{
-      sendCombo(a, c);
-      delay(800);//gap between each combo
-    }
+    IrSender.sendNEC(address, antnum3, 0);
+    delay(2000);
   }
-  Serial.println("Done sending all antenna/color combos!");
+  Serial.println("Sent!");
 }
-void loop() {//if we want it to loop forever move the for loops in here
+
+void loop() {
 }
